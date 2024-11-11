91ad6a847aa30c7cfe6db13bc7f78013108d3944
20136046 <20136046@tafe.wa.edu.au>
2024-11-11 23:55:54 +0800

    feat: add init and str methods in CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,11 @@
 class CarPark:
-    pass
+
+    def __init__(self, location="unknown", capacity=0, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):
+        return f"Car Park Information\nlocation: {self.location}\nCapacity: {self.capacity}\n"
