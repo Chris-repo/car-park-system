79f8a893f0a7c3d3bb6f4234a87b4a4f78742df1
20136046 <20136046@tafe.wa.edu.au>
2024-11-12 16:37:43 +0800

    fix: rename type to variant

--- src/sensor.py
+++ src/sensor.py
@@ -1,18 +1,18 @@
 class Sensor:
-    type = "Sensor"
+    variant = "Sensor"
     def __init__(self, id=0, is_active=False, car_park=None):
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
     def __str__(self):
-        return f"{(self.type)}\nid: {self.id}\nStatus: {self.is_active}\n"
+        return f"{(self.variant)}\nid: {self.id}\nStatus: {self.is_active}\n"
 
 
 class EntrySensor(Sensor):
-    type = "Entry Sensor"
+    variant = "Entry Sensor"
 
 
 class ExitSensor(Sensor):
-    type = "Exit Sensor"
+    variant = "Exit Sensor"
 
