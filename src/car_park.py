302bbfebc0eaf026d88b5d3208973f9865b08a58
20136046 <20136046@tafe.wa.edu.au>
2024-11-14 19:15:22 +0800

    test: add car park config tests

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 from datetime import datetime
 from sensor import Sensor
@@ -6,6 +7,8 @@ from display import Display
 
 class CarPark:
     _available_bays = 0
+    _default_log_file = "../logs/car_park_log.txt"
+    _default_config_file = "../config/car_park_config.json"
 
     def __init__(
             self, location="unknown",
@@ -13,7 +16,8 @@ class CarPark:
             plates=None,
             sensors=None,
             displays=None,
-            log_file="../logs/car_park_log.txt"
+            log_file=_default_log_file,
+            config_file=_default_config_file
     ):
         self.location = location
         self.capacity = capacity
@@ -22,6 +26,8 @@ class CarPark:
         self.displays = displays or []
         self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
         self.log_file.touch(exist_ok=True)
+        self.config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        self.config_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car Park Information\nlocation: {self.location}\nCapacity: {self.available_bays} of {self.capacity}\n"
@@ -65,3 +71,15 @@ class CarPark:
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
             f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
+    def write_config(self):
+        with open(self.config_file, "w") as f:
+            json.dump({"location": self.location, "capacity": self.capacity, "log_file": str(self.log_file)}, f)
+
+    @classmethod
+    def from_config(cls, config_file=_default_config_file):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -6,12 +6,16 @@ from car_park import CarPark
 class TestCarPark(unittest.TestCase):
     TEST_DEFAULT_LOG_FILEPATH = "../logs/car_park_log.txt"
     TEST_LOG_FILEPATH = "new_log.txt"
+    TEST_CONFIG_FILEPATH = "test_config.json"
+
     def setUp(self):
         self.car_park = CarPark("123 Example Street", 100)
         self.car_park_with_log = CarPark("123 Example Street", 100, log_file=self.TEST_LOG_FILEPATH)
+        self.car_park_with_config = CarPark("123 Example Street", 100, config_file=self.TEST_CONFIG_FILEPATH)
 
     def tearDown(self):
         Path(self.TEST_LOG_FILEPATH).unlink(missing_ok=True)
+        Path(self.TEST_CONFIG_FILEPATH).unlink(missing_ok=True)
 
     def test_car_park_initialized_with_all_attributes(self):
         self.assertIsInstance(self.car_park, CarPark)
@@ -62,19 +66,37 @@ class TestCarPark(unittest.TestCase):
         self.car_park_with_log.add_car("NEW-001")
         with self.car_park_with_log.log_file.open() as f:
             last_line = f.readlines()[-1]
-        self.assertIn("NEW-001", last_line) # check plate entered
-        self.assertIn("entered", last_line) # check description
-        self.assertIn("\n", last_line) # check entry has a new line
+        self.assertIn("NEW-001", last_line)  # check plate entered
+        self.assertIn("entered", last_line)  # check description
+        self.assertIn("\n", last_line)  # check entry has a new line
 
     def test_car_logged_when_exiting(self):
         self.car_park_with_log.add_car("NEW-001")
         self.car_park_with_log.remove_car("NEW-001")
         with self.car_park_with_log.log_file.open() as f:
             last_line = f.readlines()[-1]
-        self.assertIn("NEW-001", last_line) # check plate entered
-        self.assertIn("exited", last_line) # check description
-        self.assertIn("\n", last_line) # check entry has a new line
+        self.assertIn("NEW-001", last_line)  # check plate entered
+        self.assertIn("exited", last_line)  # check description
+        self.assertIn("\n", last_line)  # check entry has a new line
+
+    def test_config_file_created(self):
+        self.assertTrue(Path(self.TEST_CONFIG_FILEPATH).exists())
+
+    def test_config_write(self):
+        self.car_park_with_config.write_config()
+        with self.car_park_with_config.config_file.open() as f:
+            last_line = f.readlines()[-1]
+        self.assertIn("123 Example Street", last_line)
+
+    def test_car_park_object_created_from_config(self):
+        test_config = Path(self.TEST_CONFIG_FILEPATH)
+        test_config.write_text('{"location":"123 Test Street","capacity":69,"log_file":"new_log.txt"}')
+        test_class_from_config = CarPark.from_config(self.TEST_CONFIG_FILEPATH)
+        self.assertIsInstance(test_class_from_config, CarPark)
+        self.assertEqual("123 Test Street", test_class_from_config.location)
+        self.assertEqual(69, test_class_from_config.capacity)
 
 
 if __name__ == "__main__":
+    unittest.TestLoader.sortTestMethodsUsing = None
     unittest.main()
