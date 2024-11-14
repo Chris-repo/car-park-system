588235c12e1da3c8a84e05dd3d103717ddf77887
20136046 <20136046@tafe.wa.edu.au>
2024-11-14 15:01:40 +0800

    feat: car park activity log to file

--- src/car_park.py
+++ src/car_park.py
@@ -5,12 +5,20 @@ from display import Display
 class CarPark:
     _available_bays = 0
 
-    def __init__(self, location="unknown", capacity=0, plates=None, sensors=None, displays=None):
+    def __init__(
+            self, location="unknown",
+            capacity=0,
+            plates=None,
+            sensors=None,
+            displays=None,
+            log_file="../logs/car_park_log.txt"
+    ):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
+        self.log_file = log_file
 
     def __str__(self):
         return f"Car Park Information\nlocation: {self.location}\nCapacity: {self.available_bays} of {self.capacity}\n"
